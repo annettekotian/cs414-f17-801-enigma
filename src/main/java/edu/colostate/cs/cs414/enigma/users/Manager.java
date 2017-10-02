51839dc2de3d351751d0cdb983e86ce74a18b005
icziemba <icziemba@gmail.com>
2017-10-02 18:45:58 -0400

    Manager servlet and filter.

--- src/main/java/edu/colostate/cs/cs414/enigma/users/Manager.java
+++ src/main/java/edu/colostate/cs/cs414/enigma/users/Manager.java
@@ -1,33 +1,23 @@
 package edu.colostate.cs.cs414.enigma.users;
 
 import java.io.IOException;
-import java.util.*;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.google.gson.Gson;
 
 /**
- * Servlet implementation class Login
+ * Servlet implementation class Manager
  */
-@WebServlet(
-		description = "Gym system login page", 
-		urlPatterns = { 
-				"/Login", 
-				"/login"
-		})
-public class Login extends HttpServlet {
+@WebServlet({ "/Manager/ui", "/manager/ui" })
+public class Manager extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Login() {
+    public Manager() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -36,7 +26,7 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("index.jsp");
+		request.getRequestDispatcher("/WEB-INF/views/manager/manager.jsp").forward(request, response);
 	}
 
 	/**
@@ -44,24 +34,7 @@ public class Login extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String userName = request.getParameter("userName");
-		String password = request.getParameter("password");
-
-		Map<String, String> values = new HashMap<String, String>();
-		if(User.authenticate(userName, password)) {
-			User user = User.findUser(userName);
-			HttpSession session = request.getSession(true);
-			session.setAttribute("level", user.getUserLevel().getDescription());
-			
-			values.put("isLoggedIn", "true");
-			values.put("url", "manager/ui");
-		}
-		else{
-			values.put("isLoggedIn", "false");
-		}
-		
-		response.setContentType("application/json");
-		response.getWriter().write(new Gson().toJson(values));
+		doGet(request, response);
 	}
 
 }
