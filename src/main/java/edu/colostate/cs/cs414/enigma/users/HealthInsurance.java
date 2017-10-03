4aed946e4fd18bf0fd01f60a0e26cd2fbad3e390
Ian Ziemba <icziemba@gmail.com>
2017-10-02 21:07:45 -0500

    HealthInsurance JPA class.

---
+++ src/main/java/edu/colostate/cs/cs414/enigma/users/HealthInsurance.java
@@ -0,0 +1,44 @@
+package edu.colostate.cs.cs414.enigma.users;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the health_insurance database table.
+ * 
+ */
+@Entity
+@Table(name="health_insurance")
+@NamedQuery(name="HealthInsurance.findAll", query="SELECT h FROM HealthInsurance h")
+public class HealthInsurance implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(unique=true, nullable=false)
+	private int id;
+
+	@Column(nullable=false, length=255)
+	private String description;
+
+	public HealthInsurance() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
